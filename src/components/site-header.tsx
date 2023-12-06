7ebb97c085f172b81f948a5285a5cf6ed1772512
Deepanshu Goel <symaticvisuals02@gmail.com>
2023-12-06 14:43:41 +0530

    Change

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -4,25 +4,17 @@ import { siteConfig } from "@/config/site";
 import { ConnectButton } from "arweave-wallet-kit";
 import { MainNav } from "@/components/main-nav";
 
-import { useBreakpoint } from "@/hooks/useBreakpoint";
 import { ThemeSwitcher } from "./theme-switcher";
 
 export function SiteHeader() {
-  const { isAboveSm } = useBreakpoint("sm");
-
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            <div className="mr-4">
-              <ThemeSwitcher />
-            </div>
-            <ConnectButton
-              showBalance={isAboveSm}
-              showProfilePicture={isAboveSm}
-            />
+            <ThemeSwitcher />
+            <ConnectButton showBalance={true} showProfilePicture={true} />
           </nav>
         </div>
       </div>
